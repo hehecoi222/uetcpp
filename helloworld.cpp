48cf9274e91e701a74b7f21f9caa4036467398b7
hehecoi222 <hehecoi@outlook.com>
2021-10-08 11:07:26 +0700

    chicken gang with dog

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,19 +1,26 @@
-//  Tinh tong hieu 2 so nguyen
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int soThu1,soThu2;
-    cout << "Nhap vao so nguyen dau tien: ";
-    cin >> soThu1;
-    cout << "Nhap so nguyen tiep theo: ";
-    cin >>soThu2;
-    int tong,hieu;
-    tong = soThu1 + soThu2;
-    hieu = soThu1 - soThu2;
-    cout << "Tong: " << tong << endl;
-    cout << "Hieu: " << hieu << endl;
-    return 0;
+    int c1, c2;
+    int a = 0;
+    int b = 0;
+
+    cin >> c1 >> c2;
+
+    if (c1 > c2)
+    {
+        printf("invalid");
+    }
+    if ((((4 * c1) - c2) % 2 == 0) && ((((4 * c1) - c2) / 2) <= c1))
+    {
+        b = ((4 * c1) - c2) / 2;
+        a = c1 - b;
+        printf("chicken = %d, dog = %d", b, a);
+    }
+    else
+    {
+        printf("invalid");
+    }
 }
