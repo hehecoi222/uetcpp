d0e5cb1ea9dc9b799ee33b5606f0438675656234
hehecoi222 <hehecoi@outlook.com>
2021-10-09 19:26:26 +0700

    Cal fibo but not right :<

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,26 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define phi(n) pow((1 - sqrt(5)) / 2, n)
+#define PHI(n) pow((1 + sqrt(5)) / 2, n)
 using namespace std;
 
 int main()
 {
-    int c1, c2;
-    int a = 0;
-    int b = 0;
-
-    cin >> c1 >> c2;
-
-    if (c1 > c2)
-    {
-        printf("invalid");
-    }
-    if ((((4 * c1) - c2) % 2 == 0) && ((((4 * c1) - c2) / 2) <= c1))
-    {
-        b = ((4 * c1) - c2) / 2;
-        a = c1 - b;
-        printf("chicken = %d, dog = %d", b, a);
-    }
-    else
-    {
-        printf("invalid");
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    double a;
+    a = (PHI(n) + phi(n)) / sqrt(5);
+    cout << int(a);
 }
