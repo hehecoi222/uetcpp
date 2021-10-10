eada1eb2c1497579143594f79878b8d66fa26b18
hehecoi222 <hehecoi@outlook.com>
2021-10-10 09:36:29 +0700

    Ready

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
-#define phi(n) pow((1 - sqrt(5)) / 2, n)
-#define PHI(n) pow((1 + sqrt(5)) / 2, n)
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    cin >> n;
-    double a;
-    a = (PHI(n) + phi(n)) / sqrt(5);
-    cout << int(a);
+
+    
 }
