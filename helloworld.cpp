e6ced423d50e7a5bbc72da0a5247154b63d9bc4f
hehecoi222 <hehecoi@outlook.com>
2021-10-14 00:05:42 +0700

    count string

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    string s, t;
+    cin >> s >> t;
+    int count = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == t[i])
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
 }
