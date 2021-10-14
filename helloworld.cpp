c630284da839833e8796f17ed5ca49ecb357a977
hehecoi222 <hehecoi@outlook.com>
2021-10-14 08:23:14 +0700

    above average test

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,15 +6,20 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string s, t;
-    cin >> s >> t;
-    int count = 0;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == t[i])
-        {
-            count++;
-        }
+    int n;
+    cin >>n;
+    vector <double> narray;
+    double sum =0;
+    for (int i =0;i<n;i++){
+	    double temp;
+	    cin >> temp;
+	    sum += temp;
+	    narray.push_back(temp);
+	}
+    sum /= n;
+    for (int i =0; i<n;i++){
+    	if (narray[i] >= sum) {
+		printf("%.2f ",narray[i]);
+	}
     }
-    printf("%d", count);
 }
