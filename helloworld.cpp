f89265d42ea93b400c9a2cd02d37f87593be3c2a
hehecoi222 <hehecoi@outlook.com>
2021-10-14 11:23:43 +0700

    check result

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,25 +7,15 @@ int main()
     cin.tie(NULL);
 
    int n;
-   cin >>n;
-   vector<int> inp;
-   for (int i =0; i<n;i++){
-   	int temp;
-	cin >> temp;
-	inp.push_back(temp);
-   }
-   vector<int> seq;
-   vector<bool> check;
-   seq.assign(100000, 0);
-   check.assign(100000, false);
+   cin >> n;
+   if (n<1 || n >30)
+       return 0;
    for (int i = 0; i < n; i++)
    {
-       seq[inp[i]]++;
-   }
-   for (int i=0;i<n;i++){
-    if (seq[inp[i]] == 0 && check[inp[i]])
-        continue;
-    printf("%d %d\n", inp[i], seq[inp[i]]);
-    check[inp[i]] = true;
+       int temp;
+       cin >> temp;
+       if (temp >= 0) {
+           printf("%d ", i + 1);
+       }
    }
 }
