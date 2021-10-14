e4673b10aa1839512799aeced8fb7e729dcea2fe
hehecoi222 <hehecoi@outlook.com>
2021-10-14 20:48:05 +0700

    still working

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,22 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
+double power(double x,int n) {
+    double temp=1;
+    if (n == 0)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
+        return temp;
+    }
+    for (;(n!= 0 && n!=1); )
+    {
+        if (n % 2 ==0) {
+            temp *= x * x;
+            n -= 2;
+        } else if (n % 2 ==1) {
+            temp *= x;
+            n--;
+        }
+    }
+    return temp;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-   int n;
-   cin >> n;
-   double sum =0;
-   int man = 0;
-   for (int i = 0; i < n; i++)
-   {
-       double temp;
-       int hs;
-       cin >> temp >> hs;
-       sum += temp*hs;
-       man += hs;
-   }
-   printf("%.2f", sum / man);
+    long x;
+    int n;
+    cin >> x >> n;
+    printf("%d", (int)(x * (power((1 + 0.007), n))));
 }
