a706ffbe92684f1f0ec5465c348d5d07c746cd2d
hehecoi222 <hehecoi@outlook.com>
2021-10-14 17:50:05 +0700

    idea

--- divide.cpp
+++ divide.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool chiahet(int a, int b){
-	if (a%b ==0 || b%a==0) {
+bool chiahet(int a, int b);
+
+int main() {
+	cout << chiahet(2,5) << endl;
+	return 0;
+}
+
+bool chiahet(int a, int b) {
+	if (a%b==0 || b%a==0) {
 		return true;
 	}
 	return false;
-}
-
-int main() {
-	cout << chiahet(2,4) << endl;
-	return 0;
 }
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,12 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    
+int main(){
+    int n=2;
+    double sum=0;
+    for (int i=1; i<=n;i++) {
+        sum += ((double)n)/((double)n+1);
+    }
+    printf("%.5f",sum);
 }
