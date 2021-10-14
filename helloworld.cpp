27b70b4491fafd1590b1cf98f455bbd74b90a2f7
hehecoi222 <hehecoi@outlook.com>
2021-10-14 11:12:26 +0700

    frequency

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,20 +6,26 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >>n;
-    vector <double> narray;
-    double sum =0;
-    for (int i =0;i<n;i++){
-	    double temp;
-	    cin >> temp;
-	    sum += temp;
-	    narray.push_back(temp);
-	}
-    sum /= n;
-    for (int i =0; i<n;i++){
-    	if (narray[i] >= sum) {
-		printf("%.2f ",narray[i]);
-	}
-    }
+   int n;
+   cin >>n;
+   vector<int> inp;
+   for (int i =0; i<n;i++){
+   	int temp;
+	cin >> temp;
+	inp.push_back(temp);
+   }
+   vector<int> seq;
+   vector<bool> check;
+   seq.assign(100000, 0);
+   check.assign(100000, false);
+   for (int i = 0; i < n; i++)
+   {
+       seq[inp[i]]++;
+   }
+   for (int i=0;i<n;i++){
+    if (seq[inp[i]] == 0 && check[inp[i]])
+        continue;
+    printf("%d %d\n", inp[i], seq[inp[i]]);
+    check[inp[i]] = true;
+   }
 }
