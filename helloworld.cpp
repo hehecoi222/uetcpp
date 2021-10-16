93076b40968073bddd9583bcf3f93e8ee1960ff7
hehecoi222 <hehecoi@outlook.com>
2021-10-16 22:03:48 +0700

    perf num

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,38 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-double power(double x,int n) {
-    double temp=1;
-    if (n == 0)
-    {
-        return 1;
-    }
-    else if (n == 1)
-    {
-        return temp;
-    }
-    for (;(n!= 0 && n!=1); )
-    {
-        if (n % 2 ==0) {
-            temp *= x * x;
-            n -= 2;
-        } else if (n % 2 ==1) {
-            temp *= x;
-            n--;
-        }
-    }
-    return temp;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    long x;
     int n;
-    cin >> x >> n;
-    printf("%d", (int)(x * (power((1 + 0.007), n))));
+    cin >> n;
+
+    int sum = 1;
+    for (int i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i + n / i;
+            if (sum > n)
+            {
+                printf("no");
+                return 0;
+            }
+        }
+    }
+    if (sum = n)
+    {
+        printf("yes");
+    }
+    else
+        printf("no");
 }
