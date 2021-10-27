fce84acabacba5111c14076b07081ef71a05e202
hehecoi222 <hehecoi@outlook.com>
2021-10-27 11:04:07 +0700

    String with map, slow ver

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,52 +1,57 @@
 #include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
-bool check(int n)
-{  
-   if (n==2) {
-      return true;
-   }else if (n < 2 || n % 2 == 0)
-      return false;
-   for (int i = 3; i <= sqrt(n); i += 2)
-   {
-      if (n % i == 0)
-         return false;
-   }
-   return true;
+bool checkMap(string s, unordered_map<char,int> Map){
+    for (auto i=s.begin();i!=s.end();i++){
+        Map[*i]--;
+    }
+    for (auto i=Map.begin();i!=Map.end();i++){
+        if (i->second > 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-int main()
-{
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+int checkString(string s1, string s2,int j){
+    unordered_map<char,int> Map;
+    for (auto i=s1.begin();i!=s1.end();i++){
+        if (Map[*i] >100) Map[*i]=0;
+        Map[*i]++;
+    }
+    int sum=0;
+    for (int i=j+1;i<s2.length()-s1.length()+1;i++){
+        string temp=s2.substr(i,s1.length());
+        if (checkMap(temp,Map)) {
+            sum++;
+        }
+    }
+    return sum;
+}
 
-   int n;
-   cin >> n;
-   vector<int> A;
-   unordered_map<int, bool> PrimeMap;
-   vector<int> Prime;
-   for (int i = 0; i < n; i++)
-   {
-      int temp;
-      cin >> temp;
-      A.push_back(temp);
-      PrimeMap[i] = check(temp);
-      if (PrimeMap[i] == true)
-      {
-         Prime.push_back(temp);
+
+int check() {
+  string s;
+  cin >> s;
+  int sum=0;
+
+  for (int i=1;i< s.length();i++){
+      for (int j=0;j<s.length()-i;j++){
+          string temp = s.substr(j,i);
+          sum += checkString(temp,s,j);
       }
-   }
-   sort(Prime.begin(), Prime.end());
-   for (int i = 0, j = 0; i < n; i++)
-   {
-      if (PrimeMap[i])
-      {
-         A[i] = Prime[j];
-         j++;
-      }
-   }
-   for (auto i = A.begin(); i != A.end(); i++)
-   {
-      printf("%d ", *i);
-   }
+  }
+  return sum;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int q;
+  cin >> q;
+  for (int i = 0; i < q; i++) {
+    cout << check() << endl;
+  }
 }
