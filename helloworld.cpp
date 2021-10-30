29b809592d98fcc672c2ba5348f1add74d330e7c
hehecoi222 <hehecoi@outlook.com>
2021-10-30 13:25:34 +0700

    a^3 + b^3 = c^3 + d^3

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,80 +1,31 @@
 #include <bits/stdc++.h>
-#include <string>
 using namespace std;
-
-bool checkMap(string s, unordered_map<char, int> Map)
-{
-    for (auto i = s.begin(); i != s.end(); i++)
-    {
-        Map[*i]--;
-    }
-    for (auto i = Map.begin(); i != Map.end(); i++)
-    {
-        if (i->second > 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-int checkString(string s1, string s2, int j)
-{
-    unordered_map<char, int> Map;
-    for (auto i = s1.begin(); i != s1.end(); i++)
-    {
-        if (Map[*i] > 100)
-            Map[*i] = 0;
-        Map[*i]++;
-    }
-    int sum = 0;
-    for (int i = j + 1; i < s2.length() - s1.length() + 1; i++)
-    {
-        string temp = s2.substr(i, s1.length());
-        if (temp == s1)
-        {
-            sum++;
-        }
-        else if (s1.length() == 1 && temp != s1)
-        {
-            continue;
-        }
-        else if (checkMap(temp, Map))
-        {
-            sum++;
-        }
-    }
-    return sum;
-}
-
-int check()
-{
-    string s;
-    cin >> s;
-    int sum = 0;
-
-    for (int i = 1; i < s.length(); i++)
-    {
-        for (int j = 0; j < s.length() - i; j++)
-        {
-            string temp = s.substr(j, i);
-            sum += checkString(temp, s, j);
-        }
-    }
-    return sum;
-}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int q;
-    cin >> q;
-    for (int i = 0; i < q; i++)
-    {
-        cout << check() << endl;
-        int n;
-        cin >> n;
+    int n;
+    cin >> n;
+
+    for (int a=1;a<n;a++){
+        if ((a*a*a)*2 > n) break;
+        for (int b=a+1;b<n;b++){
+            long tong = a*a*a + b*b*b;
+            if (tong > n) {
+                break;
+            }
+            for (int c=a+1;c<b;c++){
+                for (int d =c+1;d<n;d++){
+                    if (c*c*c + d*d*d > tong) {
+                        break;
+                    } else if (c*c*c + d*d*d == tong){
+                        cout << c*c*c + d*d*d << " " << a << " " << b << " " << c << " " << d << endl;
+                    }
+                }
+            }
+        }
     }
+    return 0;
 }
