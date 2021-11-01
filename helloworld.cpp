fdf237554698f19c1c95dc8204849cf61dec8a44
hehecoi222 <hehecoi@outlook.com>
2021-11-01 16:51:10 +0700

    Sub code

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,26 +6,18 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
-
-    for (int a=1;a<n;a++){
-        if ((a*a*a)*2 > n) break;
-        for (int b=a+1;b<n;b++){
-            long tong = a*a*a + b*b*b;
-            if (tong > n) {
-                break;
-            }
-            for (int c=a+1;c<b;c++){
-                for (int d =c+1;d<n;d++){
-                    if (c*c*c + d*d*d > tong) {
-                        break;
-                    } else if (c*c*c + d*d*d == tong){
-                        cout << c*c*c + d*d*d << " " << a << " " << b << " " << c << " " << d << endl;
-                    }
-                }
-            }
-        }
+    string s;
+    cin >> s;
+    int sum = 0;
+    for (int i = 10; i > 1; i--){
+        sum += (s[s.length()-i+1] - '0') * i;
     }
-    return 0;
+    int c = 11-(sum % 11);
+    if (c != 0 && c<10) {
+    cout << s[0] << "-" << s.substr(1,3) << "-" << s.substr(4,5) << "-" << c; 
+    } else {
+        
+    cout << s[0] << "-" << s.substr(1,3) << "-" << s.substr(4,5) << "-" << 'X'; 
+    }
+        return 0;
 }
