f6245333fd0ff5e61234d00df165d4d05c408c75
hehecoi222 <hehecoi@outlook.com>
2021-11-03 07:29:43 +0700

    Matrix with vector

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,23 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int InMat(vector<vector<int>> mat, int m,int n) {
+    for (int i=0;i<m;i++){
+        vector<int> temp;
+        for (int j=0;j<n;j++){
+            int tempNum;
+            cin >> tempNum;
+            temp.push_back(tempNum);
+        }
+        mat.push_back(temp);
+    }
+    return 0;
+}
 
-    string s;
-    cin >> s;
-    int sum = 0;
-    for (int i = 10; i > 1; i--){
-        sum += (s[s.length()-i+1] - '0') * i;
+int OutMat(vector<vector<int>> mat, int m,int n) {
+    for (int i=0;i<m;i++){
+        for (int j=0;j<n;j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
     }
-    int c = 11-(sum % 11);
-    if (c != 0 && c<10) {
-    cout << s[0] << "-" << s.substr(1,3) << "-" << s.substr(4,5) << "-" << c; 
-    } else {
-        
-    cout << s[0] << "-" << s.substr(1,3) << "-" << s.substr(4,5) << "-" << 'X'; 
-    }
-        return 0;
+    return 0;
+}
+
+int main() {
+    vector<vector<int>> Set;
+    int n, m;
+    printf("Please input the number of rows and columns: ");
+    cin >> m >> n;
+    printf("Please input the matrix: ");
+    InMat(Set, m, n);
+    printf("The matrix is: ");
+    OutMat(Set, m, n);
 }
