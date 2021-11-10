9bc0e6b0b62b728ea203cab69075341e88425ad3
Hieu Nguyen <hehecoi@outlook.com>
2021-11-10 03:19:24 +0000

    null

---
+++ somepointerwork.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double *getPointerToPi(){
+    double *pi = new double;
+    *pi = 3.14;
+    return pi;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
