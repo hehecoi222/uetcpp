683716857c7d2c41c00d68043e73380beeb0febd
Hieu Nguyen <hehecoi@outlook.com>
2021-11-10 00:05:22 +0000

    rev pointer

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
-    int a,b;
-    cin >> a >> b;
-    long Power = pow(a,b);
-    cout << Power;
+    int **mat = new int*[10];
+    for (int i = 0; i < 10; i++) {
+        mat[i] = new int[10];
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
