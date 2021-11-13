2f3b74484e46496dcb20d35aa9eb520927b6b3f0
Hieu Nguyen <hehecoi@outlook.com>
2021-11-13 08:24:30 +0000

    still can't get bin file work

--- somepointerwork.cpp
+++ somepointerwork.cpp
@@ -1,15 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double *getPointerToPi(){
-    double *pi = new double;
-    *pi = 3.14;
-    return pi;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
+
+    char *path = new char[100];
+    cin >> path;
+    ifstream file(path,ios::binary);
+    if(!file.is_open())
+    {
+        cout << "File not found" << endl;
+        return 1;
+    }
+    int temp=0;
+    do {
+        char c;
+        file.get(c);
+        if (c == temp)
+            continue;
+        temp = c;
+        cout << c;
+    } while(!file.eof());
+    file.close();
 }
