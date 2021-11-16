7ad2571ea0425adb017f4099cbab135ad8670e49
Hieu Nguyen <hehecoi@outlook.com>
2021-11-16 02:35:43 +0000

    Struct embeded

--- structLearn.cpp
+++ structLearn.cpp
@@ -5,17 +5,35 @@ struct DiemSo {
     double Toan,Ly,DiemTB;
 };
 
+struct SinhVien {
+    string HoTen;
+    int NamSinh;
+    DiemSo Diem;
+};
+
 int main() {
     const int SoSV = 10;
-    DiemSo SV[SoSV];
+    SinhVien SV[SoSV];
     for (int i = 0; i < SoSV; i++) {
-        cout << "Nhap diem Toan, Ly cua sinh vien thu " << i + 1 << ": ";
-        cin >> SV[i].Toan >> SV[i].Ly;
-        SV[i].DiemTB = (SV[i].Toan + SV[i].Ly) / 2;
+        cout << "Nhap thong tin sinh vien thu " << i + 1 << endl;
+        cout << "Ho ten: ";
+        getline(cin, SV[i].HoTen);
+        cout << "Nam sinh: ";
+        cin >> SV[i].NamSinh;
+        cin.ignore();
+        cout << "Diem toan: ";
+        cin >> SV[i].Diem.Toan;
+        cout << "Diem ly: ";
+        cin >> SV[i].Diem.Ly;
+        SV[i].Diem.DiemTB = (SV[i].Diem.Toan + SV[i].Diem.Ly) / 2;
     }
-    cout << "Danh sach diem so cua cac sinh vien: " << endl;
+    cout << "Thong tin sinh vien" << endl;
     for (int i = 0; i < SoSV; i++) {
-        cout << "Sinh vien thu " << i + 1 << ": " << SV[i].Toan << " " << SV[i].Ly << " " << SV[i].DiemTB << endl;
+        cout << "Ho ten: " << SV[i].HoTen << endl;
+        cout << "Nam sinh: " << SV[i].NamSinh << endl;
+        cout << "Diem toan: " << SV[i].Diem.Toan << endl;
+        cout << "Diem ly: " << SV[i].Diem.Ly << endl;
+        cout << "Diem TB: " << SV[i].Diem.DiemTB << endl;
     }
     return 0;
 }
