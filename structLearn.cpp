bc20a25dbc32925a771786f036450f21af460484
Hieu Nguyen <hehecoi@outlook.com>
2021-11-17 01:04:25 +0000

    Da thuc chua sua

--- structLearn.cpp
+++ structLearn.cpp
@@ -1,40 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Vector {
-    double X, Y;
-    int VectorAddition(Vector &v) {
-        X += v.X;
-        Y += v.Y;
-        return 0;
-    }
+struct Dathuc {
+    int Bac;
+    vector<double> Co;
 };
 
-int Nhap(Vector &v) {
-    cout << "Nhap toa do X: ";
-    cin >> v.X;
-    cout << "Nhap toa do Y: ";
-    cin >> v.Y;
+int Nhap(Dathuc &a) {
+    cout << "Nhap bac cua dathuc: ";
+    cin >> a.Bac;
+    if (a.Bac < 0) {
+        cout << "Bac phai lon hon hoac bang 0" << endl;
+        return 1;
+    }
+    cout << "Nhap cac co cua dathuc: ";
+    for (int i = 0; i <= a.Bac; i++) {
+        double temp;
+        cin >> temp;
+        a.Co.push_back(temp);
+    }
     return 0;
 }
 
-int Xuat(Vector v) {
-    cout << "(" << v.X << ", " << v.Y << ")" << endl;
+int Xuat(Dathuc a) {
+    cout << "Dathuc: ";
+    for (int i = 0; i < a.Bac; i++) {
+        cout << a.Co[i] << "x^" << a.Bac - i << " + ";
+    }
+    cout << a.Co[a.Bac] << endl;
+    return 0;
+}
+
+int Tong(Dathuc a, Dathuc b, Dathuc &c) {
+    if (a.Bac > b.Bac) {
+        c.Bac = a.Bac;
+    } else {
+        c.Bac = b.Bac;
+    }
+    for (int i = 0; i <= c.Bac; i++) {
+        if (i > b.Bac) {
+            c.Co.push_back(a.Co[i]);
+            continue;
+        } else if (i > a.Bac) {
+            c.Co.push_back(b.Co[i]);
+            continue;
+        }
+        double temp = a.Co[i] + b.Co[i];
+        c.Co.push_back(temp);
+    }
     return 0;
 }
 
 int main() {
-    Vector v1, v2;
-    cout << "Nhap v1: ";
-    Nhap(v1);
-    cout << "Nhap v2: ";
-    Nhap(v2);
-    cout << "v1: ";
-    Xuat(v1);
-    cout << "v2: ";
-    Xuat(v2);
-    cout << "Tong 2 vector: ";
-    v1.VectorAddition(v2);
-    Xuat(v1);
+    Dathuc a, b, c;
+    cout << "Nhap dathuc thu nhat: ";
+    Nhap(a);
+    cout << "Nhap dathuc thu hai: ";
+    Nhap(b);
+    cout << "Tong 2 dathuc: ";
+    Tong(a, b, c);
+    Xuat(c);
     return 0;
 }
