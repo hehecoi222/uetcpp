e5b307df3da8110d1764714e31748fa92262fc1b
Hieu Nguyen <hehecoi@outlook.com>
2021-11-17 02:35:28 +0000

    Date Month vv

--- structLearn.cpp
+++ structLearn.cpp
@@ -1,64 +1,142 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-struct Dathuc {
-    int Bac;
-    vector<double> Co;
+struct date
+{
+    int day, month, year;
+    bool IsLeapYear()
+    {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+        else
+            return false;
+    }
+    bool Valid () {
+        if (month > 0 && month <= 12) {
+            if (month == 2) {
+                if (IsLeapYear()) {
+                    if (day > 0 && day <= 29) {
+                        return true;
+                    }
+                } else {
+                    if (day > 0 && day <= 28) {
+                        return true;
+                    }
+                }
+            } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (day > 0 && day <= 30) {
+                    return true;
+                }
+            } else {
+                if (day > 0 && day <= 31) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    date(int _day, int _month, int _year)
+    {
+        // them code cua ban o day
+        day = _day;
+        month = _month;
+        year = _year;
+    }
+    void getNextDay()
+    {
+        // them code cua ban o day
+        switch (month)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+            if (day == 31)
+            {
+                day = 1;
+                month++;
+            }
+            else
+                day++;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (day == 30)
+            {
+                day = 1;
+                month++;
+            }
+            else
+                day++;
+            break;
+        case 2:
+            if (IsLeapYear())
+            {
+                if (day == 29)
+                {
+                    day = 1;
+                    month++;
+                }
+                else
+                    day++;
+            }
+            else
+            {
+                if (day == 28)
+                {
+                    day = 1;
+                    month++;
+                }
+                else
+                    day++;
+            }
+            break;
+        case 12:
+            if (day == 31)
+            {
+                day = 1;
+                month = 1;
+                year++;
+            }
+            else
+                day++;
+            break;
+        }
+    }
 };
 
-int Nhap(Dathuc &a) {
-    cout << "Nhap bac cua dathuc: ";
-    cin >> a.Bac;
-    if (a.Bac < 0) {
-        cout << "Bac phai lon hon hoac bang 0" << endl;
-        return 1;
+int main()
+{
+    // them code cua ban o day
+    string s;
+    cin >> s;
+    date d(stoi(s.substr(0, 2)), stoi(s.substr(3, 2)), stoi(s.substr(6, 4)));
+    if (d.Valid())
+    {
+       d.getNextDay();
+       if (d.Valid())
+       {
+       printf("%02d/%02d/%04d", d.day, d.month, d.year);
+       } else {
+           printf("INVALID");
+       }
     }
-    cout << "Nhap cac co cua dathuc: ";
-    for (int i = 0; i <= a.Bac; i++) {
-        double temp;
-        cin >> temp;
-        a.Co.push_back(temp);
+    else
+    {
+        cout << "INVALID";
     }
-    return 0;
-}
-
-int Xuat(Dathuc a) {
-    cout << "Dathuc: ";
-    for (int i = 0; i < a.Bac; i++) {
-        cout << a.Co[i] << "x^" << a.Bac - i << " + ";
-    }
-    cout << a.Co[a.Bac] << endl;
-    return 0;
-}
-
-int Tong(Dathuc a, Dathuc b, Dathuc &c) {
-    if (a.Bac > b.Bac) {
-        c.Bac = a.Bac;
-    } else {
-        c.Bac = b.Bac;
-    }
-    for (int i = 0; i <= c.Bac; i++) {
-        if (i > b.Bac) {
-            c.Co.push_back(a.Co[i]);
-            continue;
-        } else if (i > a.Bac) {
-            c.Co.push_back(b.Co[i]);
-            continue;
-        }
-        double temp = a.Co[i] + b.Co[i];
-        c.Co.push_back(temp);
-    }
-    return 0;
-}
-
-int main() {
-    Dathuc a, b, c;
-    cout << "Nhap dathuc thu nhat: ";
-    Nhap(a);
-    cout << "Nhap dathuc thu hai: ";
-    Nhap(b);
-    cout << "Tong 2 dathuc: ";
-    Tong(a, b, c);
-    Xuat(c);
-    return 0;
 }
